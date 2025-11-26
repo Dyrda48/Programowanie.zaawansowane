b928ac48611423e9cd2ea3bf6c93269a21ae7ea3
Oleh Dyrda <odyrda841@gmail.com>
2025-11-27 00:50:10 +0100

    Dodano 2

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Befit.Models;
 
 namespace Befit.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ExerciseType> ExerciseTypes { get; set; } = default!;
+        public DbSet<TrainingSession> TrainingSessions { get; set; } = default!;
+        public DbSet<TrainingEntry> TrainingEntries { get; set; } = default!;
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
+
         {
         }
     }
